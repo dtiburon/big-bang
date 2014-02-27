ff1ba58db84f7718b6226cabf984be6e933cd398
dtiburon <aleta.dunne@gmail.com>
2014-02-27 23:26:38 +0100

    Start of a relational database

---
+++ flask/create_db.py
@@ -0,0 +1,3 @@
+from loadsite import db
+
+db.create_all()
--- flask/loadsite.py
+++ flask/loadsite.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask, render_template, request
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import mapper, relationship, backref
 import json
 from werkzeug.exceptions import ServiceUnavailable, BadRequest, InternalServerError
 
@@ -8,10 +10,24 @@ STATIC_FOLDER = "static"
 TEMPLATE_FOLDER = "templates"
 DATA_DIR = "data" # temporary place to store data w/out a database for testing
 
+# create database
+SQLALCHEMY_DATABASE_FILE = 'planeteria.db'
+
 app = Flask(__name__,
             static_url_path = STATIC_PATH,
             static_folder = STATIC_FOLDER,
             template_folder = TEMPLATE_FOLDER)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + SQLALCHEMY_DATABASE_FILE
+
+# create database object
+db = SQLAlchemy(app)
+
+from model.planet import Planet
+from model.feed import Feed
+
+Planet.feed = relationship(Feed, backref=backref("planet"))
+
 
 # to load any of the pages below, enter Flask ip address as URL + url path indicated in app route.
 # failing to do this will cause a 404 or direct you to the homepage.
