f1621ffa42c452840e5b7ab249fc65ee412ea646
dtiburon <aleta.dunne@gmail.com>
2014-04-18 16:47:15 +0200

    Comment & print statement cleanup

--- spider.py
+++ spider.py
@@ -13,28 +13,32 @@ import feedparser, datetime, time
 # pull planets from database
 all_planets = Planet.query.all()
 
+# Helper functions
+
 def check_dups(db_entries, entry_url):
-    # cross-references an entry's URL against the URLS of entries in the database
+    # cross-references an entry's URL against the URLS of entries in the database.
+    # returns True if the entry is already in the database.
     flag = False
     for db_entry in db_entries:
         if db_entry.url == entry_url:
             flag = True
         else:
             pass
-    if flag == True:
-        print "This entry is already in the database."
+    # if flag == True:
+    #     print "This entry is already in the database."
     return flag
 
+# Main function
 
 def update_planet(planet_id):
     # updates planet feed content
 
-    print "Loading feed data from database"
+    # loading feed data from database
     db_feeds = Feed.query.filter_by(planet_id=planet_id).all()
 
     # Loop through each feed in the planet to pull its data:
     for db_feed in db_feeds:
-        print "loaded feed:", db_feed.name
+        print "loading feed:", db_feed.name
 
         # set variables
         feedurl = db_feed.url 
@@ -45,22 +49,18 @@ def update_planet(planet_id):
 
         # pull all URL's of feed entries from DB for comparison
         db_entries = FeedContent.query.filter_by(feed_id=db_feed.id).all()
-        print 
 
-        print "Checking feed", feedurl
         # First time entry is pulled: create etag
         if not etag:
             # no need to check to see if feeds have been pulled since the last time, pull all feeds.
             pull = True
-            print "New feed. Never been pulled."
-            # Establish parsed feed object
+            # Create parsed feed object
             feed = feedparser.parse(feedurl) #todo: put in a try block. look up errors.
 
             try:
                 etag = feed.etag
             except AttributeError:
                 etag = u''
-                print "No new etag for this feed."
 
             # more data for new feeds
             try:
@@ -72,14 +72,11 @@ def update_planet(planet_id):
             except:
                 blog_url = u''
 
-            # store new feed data in database
-            print "feed_name:", feed_name
+            # store new feed data in database if it doesn't exist
             if not db_feed.name:
                 db_feed.name = feed_name
-            print "blog_url", blog_url
             if not db_feed.blogurl:
                 db_feed.blogurl = blog_url
-            print "etag:", etag
             if etag:
                 db_feed.etag = etag
 
@@ -87,51 +84,42 @@ def update_planet(planet_id):
             # todo: pull feed author and save to database
 
         else:
-            print "Feed has been pulled before."
             # Establish parsed feed object
             feed = feedparser.parse(feedurl, etag=etag)
             if feed.status != 304:
-                print "New feed content published since last pull"
+                print "New feed content published since last pull; saving changes"
                 pull = True
             else:
-                print "No new feed content since last pull"
-                print "Pull flag should be False:", pull
+                # No new feed content since last pull
+                pull = False
 
-        print 
         # if there are new entries to pull, pull them!
         if pull:
-            print "Now looping through pulled entries to save them."
             for i, entry in enumerate(feed.entries):
                 entry_url = feed.entries[i].link
-                print "New entry URL:", entry_url
                 
                 # cross-reference URL with URLs of entries saved to Database
                 if check_dups(db_entries, entry_url) == True:
                     # Feed has been previously saved to the database, it's a duplicate.
-                    print "Skipping this entry."
+                    pass
                 
                 else: # saved == False:
-                    print "Saving entry - it's new."
                     new_entry = FeedContent()
-                    # create association with the working feed in the database
+                    # create relationship with the working feed in the database
                     new_entry.feed = db_feed
                     new_entry.title = feed.entries[i].title
                     new_entry.url = entry_url
-                    print "Entry title:", new_entry.title
                     try:
                         parsed_date = feed.entries[i].published_parsed
                     except (AttributeError): 
                         parsed_date = feed.entries[i].updated_parsed
-                    print "Parsed date type:", type(parsed_date)
                     epoch_date = int(time.mktime(parsed_date))
-                    print "Date to epoch:", epoch_date
                     new_entry.date = epoch_date
-                    
                     
                     # Find the content in the parsed feed! It's in a different place for different feeds. Let the games begin.
                     content = u''
-                    try: # todo: is this the most likely format?
-                        # Entry content is wrapped like so - {'content':[{'value':}]}
+                    try: # todo: rearrange order of try blocks to start with most likely format (rss vs atom).
+                        # Entry content format: {'content':[{'value':}]}
                         content = feed.entries[i]['content'][0]['value']
                     except (AttributeError, KeyError):
                         if "summary_detail" in feed.entries[i]:
@@ -139,26 +127,24 @@ def update_planet(planet_id):
                         # last ditch - post description instead of content
                         elif "description" in feed.entries: 
                             content = feed.entries[i].description
-                            print "Feed description will have to suffice. "
-                            print "Description length:", len(content)
                         else:
-                            print "No content found anywhere. You're screwed."
+                            print "No entry content found in entry %s, entry date %s." % (entry_url, epoch_date)
                     if content:
                         new_entry.body = content
-                        print "Content type:", type(content)
+                    else:
+                        new_entry.body = "<p>No content found.</p>"
 
                     # Add the new entry to the database session
                     db.session.add(new_entry)
 
-                print #break after each iteration of the for-loop
-
     # commit all changes to database
         try:
             db.session.commit()
-        except IntegrityError, err:
-            print str(err)
+        except IntegrityError, err: #todo: proper error handling
+            print str(err) 
 
 # Loop through each planet and call the function to pull its feeds.
 for planet in all_planets:
-    print "Pulling feeds for Planet ID #:", planet.id
+    print "Pulling feeds for Planet: %i - %s" % (planet.id, planet.name)
     update_planet(planet.id)
+    print 
